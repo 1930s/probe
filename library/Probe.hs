7ef8d632cd97408cbbea91a8d163027b012503e1
Edoardo Rossi <edd.rossi@gmail.com>
2017-11-11 18:42:34 +0100

    Move Options in its own space

--- library/Probe.hs
+++ library/Probe.hs
@@ -8,6 +8,7 @@ module Probe (main) where
 
 import LinkStruct
 import Network
+import Options
 
 -- import Control.Arrow
 import Control.Concurrent ( forkIO
@@ -38,10 +39,6 @@ import Data.Maybe (fromJust)
 import Data.List ( find )
 import Network.URI
 import Prelude
-import System.Console.GetOpt
-import System.Environment ( getArgs
-                          , getProgName
-                          )
 import System.Exit ( exitSuccess
                    -- , ExitCode(..)
                    -- , exitWith
@@ -465,61 +462,3 @@ parseLinks = concatMap uris . L8.lines
   where uris s      = filter looksOkay (L8.splitWith isDelim s)
         isDelim c   = isControl c || c `elem` (" <>\"{}|\\^[]`" :: String)
         looksOkay s = s =~ ("https?://" :: String):: Bool
-
-data Options = Options  { optFiles :: [String]
-                        , optVerbose :: Bool
-                        , optErrors :: Bool
-                        , optWorkers :: IO Int
-                        , optOutput :: String -> IO ()
-                        }
-
-startOptions :: Options
-startOptions = Options  { optFiles = []
-                        , optVerbose = False
-                        , optErrors = False
-                        , optWorkers = return 8
-                        , optOutput = putStr
-                        }
-
-parseArgs :: IO Options
-parseArgs = do
-    args <- getArgs
-    let (actions, _nonOptions, _errors) = getOpt Permute options args
-    opts <- foldl (>>=) (return startOptions) actions
-    let Options { optFiles = files
-                , optVerbose = verbose
-                , optWorkers = workers
-                , optOutput = _output } = opts
-    when verbose (hPutStrLn stderr "Hey!")
-    when verbose (hPutStrLn stderr $ "files are: " ++ show files)
-    when verbose (workers >>= \w -> hPutStrLn stderr $ "workers are: " ++ show w)
-    return opts
-
-options :: [ OptDescr (Options -> IO Options) ]
-options = [ Option "o" ["output"]
-              (ReqArg (\arg opt -> return opt { optOutput = writeFile arg }) "FILE")
-              "Output file"
-          , Option "f" ["files"]
-              (ReqArg (\arg opt -> return opt { optFiles = [arg] }) "FILE")
-              "Output file"
-          , Option "e" ["errors"]
-              (NoArg (\opt -> return opt { optErrors = True }))
-              "Print only error messages"
-          , Option "v" ["verbose"]
-              (NoArg (\opt -> return opt { optVerbose = True }))
-              "Enable verbose messages"
-          , Option ['n'] ["concurrency"]
-              (ReqArg (\arg opt -> return opt { optWorkers = return (read arg :: Int) }) "WORKERS")
-               "Number of concurrent connections (default 16)"
-          , Option "V" ["version"]
-              (NoArg (\_ -> do
-                      hPutStrLn stderr "Version 0.0.1"
-                      exitSuccess))
-              "Print version"
-          , Option "h" ["help"]
-              (NoArg (\_ -> do
-                      prg <- getProgName
-                      hPutStrLn stderr (usageInfo prg options)
-                      exitSuccess))
-              "Show help"
-          ]
